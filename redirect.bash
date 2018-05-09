4d7279e2a2fe33bce03331475b266b2d81390940
Mike "Fox" Liu <gravityreallyhatesme@gmail.com>
2018-05-09 13:06:42 -0700

    First commit

---
+++ redirect.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+shopt -s nullglob
+cd .. || exit 1
+#if
+#for project in Fox*/.git; do
+#	project=${project%/.git}
+#	echo "Pushing project:" $project
+#	cd $project
+#	git push
+#done
+for project in Fox*/.git; do
+	project=${project%/.git}
+	echo "project:" $project
+	cd $project || exit 1
+	git submodule init
+	git submodule sync
+
+	for submodule in ?*/.git; do
+		submodule=${submodule%/.git}
+		if [ -d "../$submodule/.git" ]; then
+			echo "  submodule:" $submodule
+			cd $submodule || exit 1
+			#pwd
+			content="gitdir: ../../$submodule/.git"
+			echo $content > .git
+			git reset --hard HEAD
+			git clean -fd
+			cd ..
+		fi
+	done
+	cd ..
+done
+echo "Finished configuring and redirecting submodules"
