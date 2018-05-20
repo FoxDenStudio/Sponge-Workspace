5c0612cb8bac9b07f84880f35dc710a29eed9b95
Snowie/D4rkFly3r <npjoshf@gmail.com>
2018-05-19 19:56:38 -0700

    Tweaks

--- setup.sh
+++ setup.sh
@@ -4,22 +4,21 @@ workspace=$PWD
 
 cd .. || exit 1
 
-git clone --recursive git@github.com:SpongePowered/SpongeForge.git
+git clone --recursive git@github.com:SpongePowered/SpongeForge.git -b $1
 git clone git@github.com:FoxDenStudio/FoxCore-Common.git
 git clone git@github.com:FoxDenStudio/FoxGuard-Common.git
-git clone git@github.com:FoxDenStudio/FoxCore-Sponge.git
-git clone git@github.com:FoxDenStudio/FoxGuard-Sponge.git
+git clone git@github.com:FoxDenStudio/FoxCore-Sponge.git -b $2
+git clone git@github.com:FoxDenStudio/FoxGuard-Sponge.git -b $2
 
 cd $workspace || exit 1
 
 ./redirect.bash || exit 1
 
 cd ../SpongeForge || exit 1
-./gradlew setupDecompWorkspace idea --refresh-dependencies
-./gradlew genIntellijRuns
+./gradlew setupDecompWorkspace --refresh-dependencies
 
 cd ../FoxCore-Sponge || exit 1
-./gradlew setupDecompWorkspace idea --refresh-dependencies
+./gradlew setupDecompWorkspace --refresh-dependencies
 
 cd .. || exit 1
 cd $workspace || exit 1
