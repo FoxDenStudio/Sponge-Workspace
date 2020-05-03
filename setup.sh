a7ddb52ea63a8205ff0cdd95299190578038baae
Mike "Fox" Liu <gravityreallyhatesme@gmail.com>
2020-05-03 00:48:08 -0700

    Current config.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 pushd .. || exit 1
 
-#git clone --recursive git@github.com:SpongePowered/SpongeForge.git -b $1
+git clone --recursive git@github.com:SpongePowered/SpongeForge.git
 git clone git@github.com:FoxDenStudio/FoxCore-Common.git
 git clone git@github.com:FoxDenStudio/FoxGuard-Common.git
-#git clone git@github.com:FoxDenStudio/FoxCore-Sponge.git -b $2
+git clone git@github.com:FoxDenStudio/FoxCore-Sponge.git
 #git clone git@github.com:FoxDenStudio/FoxGuard-Sponge.git -b $2
 
 popd || exit 1
 
 ./redirect.bash || exit 1
 
-#cd ../SpongeForge || exit 1
-#./gradlew setupDecompWorkspace --refresh-dependencies
+cd ../SpongeForge || exit 1
+./gradlew setupDecompWorkspace --refresh-dependencies
 
-#cd ../FoxCore-Sponge || exit 1
-#./gradlew setupDecompWorkspace --refresh-dependencies
+cd ../FoxCore-Sponge || exit 1
+./gradlew setupDecompWorkspace --refresh-dependencies
 
